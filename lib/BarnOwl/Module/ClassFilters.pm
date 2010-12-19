dd00837a7f61a82629de469265b157839e073912
Kevin Riggle <kevinr@free-dissociation.com>
2010-12-18 20:19:06 -0500

    add :reload-classfilter and :classfilter-append

--- lib/BarnOwl/Module/ClassFilters.pm
+++ lib/BarnOwl/Module/ClassFilters.pm
@@ -52,51 +52,119 @@ Adds the :reload-classfilters command.
 END_DESC
 }
 
+my $filterdir = "$ENV{'HOME'}/.owl/classfilters";
+
+sub list_classfilter_files
+{
+    return glob "$filterdir/*" or die "Error opening directory $filterdir: $!";
+}
+
+sub list_classfilters
+{
+    return map { $1 if /([^\/]+)$/ } list_classfilter_files();
+}
+
+sub filtername_to_filename
+{
+    my $filtername = shift;
+    return "$filterdir/$filtername";
+}
+
 sub load_classfilters
 {
     my $verbose = shift;
 
-    my $filterdir = "$ENV{'HOME'}/.owl/classfilters";
-    my @filenames = glob "$filterdir/*" or die "Error opening directory $filterdir: $!";
+    my @filenames = list_classfilter_files();
     for my $filename (@filenames) {
-        open my $file, '<', $filename or die "Error opening file $filename: $!";
-
-        my @path_parts = split /\//, $filename;        # portability, what portability?
-        my $filtername = pop @path_parts;
-
-        my @filter_parts;
-        my $lines = 0;
-        while (<$file>) {
-            if (/^\s*\(/) {
-                push @filter_parts, 'or' if $lines++ > 0;
-                chomp(my $filter_section = $_);
-                push @filter_parts, $filter_section;
-                next;
-            }
-            my @parts = split /,/;
-            chomp(my $class = $parts[0]) if @parts > 0;
-            my $has_class = $class ne '*';
-            chomp(my $instance = $parts[1]) if @parts > 1;
-            my $has_instance = defined $instance && $instance ne '*';
-            chomp(my $recipient = $parts[2]) if @parts > 2;
-            my $has_recipient = defined $recipient && $recipient ne '*';
-
-            if ($has_class || $has_instance || $has_recipient) {
-                push @filter_parts, 'or' if $lines++ > 0;
-                push @filter_parts, '(';
-                push @filter_parts, "class ^(un)?$class\$" if $has_class;
-                push @filter_parts, "and" if $has_class && $has_instance;
-                push @filter_parts, "instance ^(un)?$instance\$" if $has_instance;
-                push @filter_parts, "and" if ($has_class || $has_instance) && $has_recipient;
-                push @filter_parts, "recipient ^$recipient\$" if $has_recipient;
-                push @filter_parts, ')';
-            }
-        }
-
-        BarnOwl::admin_message("classfilters filter", "filter '$filtername': @filter_parts") if $verbose;
-        BarnOwl::set_filter($filtername, "@filter_parts");
-        close $file;
+        load_classfilter_from_file($verbose, $filename);
     }
+}
+
+sub load_classfilter
+{
+    my $verbose = shift;
+    my $filtername = shift;
+
+    my $filename = filtername_to_filename($filtername);
+    load_classfilter_from_file($verbose, $filename);
+}
+
+sub load_classfilter_from_file 
+{
+    my $verbose = shift;
+    my $filename = shift;
+
+    open my $file, '<', $filename or die "Error opening file $filename: $!";
+
+    my @path_parts = split /\//, $filename;        # portability, what portability?
+    my $filtername = pop @path_parts;
+
+    my @filter_parts;
+    my $lines = 0;
+    while (<$file>) {
+        if (/^\s*\(/) {
+            push @filter_parts, 'or' if $lines++ > 0;
+            chomp(my $filter_section = $_);
+            push @filter_parts, $filter_section;
+            next;
+        }
+        my @parts = split /,/;
+        chomp(my $class = $parts[0]) if @parts > 0;
+        my $has_class = $class ne '*';
+        chomp(my $instance = $parts[1]) if @parts > 1;
+        my $has_instance = defined $instance && $instance ne '*';
+        chomp(my $recipient = $parts[2]) if @parts > 2;
+        my $has_recipient = defined $recipient && $recipient ne '*';
+
+        if ($has_class || $has_instance || $has_recipient) {
+            push @filter_parts, 'or' if $lines++ > 0;
+            push @filter_parts, '(';
+            push @filter_parts, "class ^(un)?$class\$" if $has_class;
+            push @filter_parts, "and" if $has_class && $has_instance;
+            push @filter_parts, "instance ^(un)?$instance\$" if $has_instance;
+            push @filter_parts, "and" if ($has_class || $has_instance) && $has_recipient;
+            push @filter_parts, "recipient ^$recipient\$" if $has_recipient;
+            push @filter_parts, ')';
+        }
+    }
+
+    BarnOwl::admin_message("classfilters filter", "filter '$filtername': @filter_parts") if $verbose;
+    BarnOwl::set_filter($filtername, "@filter_parts");
+    close $file;
+}
+
+sub classfilter_append
+{
+    my $verbose = shift;
+    my $filtername = shift;
+    my @filter = @_;
+
+    my @classfilters = list_classfilters();
+    BarnOwl::admin_message("foobar", "$filtername in: @classfilters");
+    if (!grep(/^$filtername$/, @classfilters)) {
+        BarnOwl::message("Error: No filter named '$filtername'.");
+        return 0;
+    }
+
+    my $filename = filtername_to_filename($filtername);
+    open my $file, '>>', $filename or die "Error opening file $filename: $!";
+    print {$file} "@filter\n";
+    close $file;
+    load_classfilter_from_file($verbose, $filename);
+    return 1;
+}
+
+sub startup_load_classfilters {
+    my $is_reload = shift;
+
+    load_classfilters(0);
+}
+
+eval {
+    $BarnOwl::Hooks::startup->add("BarnOwl::Module::ClassFilters::startup_load_classfilters");
+};
+if($@) {
+    $BarnOwl::Hooks::startup->add(\&startup_load_classfilters);
 }
 
 sub cmd_reload_classfilters {
@@ -116,20 +184,63 @@ BarnOwl::new_command('reload-classfilters' => \&cmd_reload_classfilters, {
     description         => 'Reload filters from ~/.owl/classfilters.'
     . "\n-v (verbose), if provided, lists the filters as they are created."
     });
-    
 
-sub startup_load_classfilters {
-    my $is_reload = shift;
-
-    load_classfilters(0);
+sub cmd_reload_classfilter {
+    my $cmd = shift;
+    my $arg1 = shift;
+    my $arg2 = shift;
+    if (defined $arg1) {
+        if ($arg1 eq '-v') {
+            if (defined $arg2) {
+                load_classfilter(1, $arg2);
+            } else {
+                die "Must specify a filter to reload";
+            }
+        } else {
+            load_classfilter(0, $arg1);
+        }
+    } else {
+        die "Must specify a filter to reload";
+    }
+    BarnOwl::message("Filter loaded.");
 }
 
-eval {
-    $BarnOwl::Hooks::startup->add("BarnOwl::Module::ClassFilters::startup_load_classfilters");
-};
-if($@) {
-    $BarnOwl::Hooks::startup->add(\&startup_load_classfilters);
+BarnOwl::new_command('reload-classfilter' => \&cmd_reload_classfilter, {
+    summary             => 'Reload a specified filter from file in ~/.owl/classfilters',
+    usage               => 'reload-classfilter [-v] NAME',
+    description         => 'Reload a specified filter from ~/.owl/classfilters.'
+    . "\n-v (verbose), if provided, displays the filter created."
+    });
+
+sub cmd_classfilter_append {
+    my $cmd = shift;
+    my $arg1 = shift;
+    my $arg2 = shift;
+    my $r = 0;
+    if (defined $arg1) {
+        if ($arg1 eq '-v') {
+            if (defined $arg2) {
+                $r = classfilter_append(1, $arg2, @_);
+            } else {
+                BarnOwl::message("Error: Must specify a filter to append to");
+                return;
+            }
+        } else {
+            $r = classfilter_append(0, $arg1, $arg2, @_);
+        }
+    } else {
+        BarnOwl::message("Error: Must specify a filter to append to");
+        return;
+    }
+    BarnOwl::message("Filter loaded.") if $r;
 }
+
+BarnOwl::new_command('classfilter-append' => \&cmd_classfilter_append, {
+    summary             => 'Append a line to the filter and file in ~/.owl/classfilters',
+    usage               => 'classfilter-append [-v] NAME FILTER',
+    description         => 'Append a line to the filter and file in ~/.owl/classfilters'
+    . "\n-v (verbose), if provided, displays the filter created."
+    });
 
 
 =head1 AUTHOR
